44c599023be72b02e1b85294da9471fc90a82485
Mickael Faivre-Maçon <faivrem@gmail.com>
2009-11-20 23:10:12 +0100

    fix

--- app/models/app_mailer.rb
+++ app/models/app_mailer.rb
@@ -11,11 +11,12 @@ class AppMailer < ActionMailer::Base
   
   def invitation(from,user,msg,pwd)
     @subject    = "[Swap!] #{from.name} invited you on Swap!"
-    @body["from"] = from
+    @body["f"] = from
     @body["user"] = user
     @body["msg"] = msg
     @body["pwd"] = pwd
     @recipients = user.email
+    @from       = "\"#{from.name}\" <#{from.email}>"
     @sent_on    = Time.now
     @headers    = {}
     content_type "text/html"
